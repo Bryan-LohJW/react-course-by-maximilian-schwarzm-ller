f3309bf97baf92591fa3d67da922ba31009b62ab
Bryan Personal <bryanlohjw97@gmail.com>
2023-02-06 17:16:53 +0800

    section 18 - redux (complete)

--- 15-redux-demo-project/src/store/index.js
+++ 15-redux-demo-project/src/store/index.js
@@ -1,54 +1,10 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+import counterReducer from './counter';
+import authReducer from './auth';
 
-const counterSlice = createSlice({
-	name: 'counter',
-	initialState,
-	reducers: {
-		increment(state) {
-			state.counter++;
-		},
-		decrement(state) {
-			state.counter--;
-		},
-		increase(state, action) {
-			state.counter += action.amount;
-		},
-		toggleCounter(state) {
-			state.showCounter = !state.showcounter;
-		},
-	},
+const store = configureStore({
+	reducer: { counter: counterReducer, auth: authReducer },
 });
-
-const counterReducer = (state = initialState, action) => {
-	if (action.type === 'increment') {
-		const newState = JSON.parse(JSON.stringify(state));
-		newState.counter = newState.counter + 1;
-		return newState;
-	}
-	if (action.type === 'increase') {
-		return {
-			counter: state.counter + action.amount,
-			showCounter: state.showCounter,
-		};
-	}
-	if (action.type === 'decrement') {
-		return {
-			counter: state.counter - 1,
-			showCounter: state.showCounter,
-		};
-	}
-	if (action.type === 'toggle') {
-		return {
-			showCounter: !state.showCounter,
-			counter: state.counter,
-		};
-	}
-	return state;
-};
-
-const store = createStore(counterReducer);
 
 export default store;
